8be5c73881ff003359310c13590ee9c09ab0d0f5
sahil raj <sahilrajsingh01@outlook.com>
2024-10-28 14:34:47 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// require ('dotenv').config()
+ import dotenv from "dotenv"
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import{dotenv} from ".dotenv/config";
+import connectDB from "./db/index.js";
+dotenv.config();
+
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
